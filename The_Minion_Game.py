a07ceb7857ef05a62a4a1a26a1ee973ec0129e11
Ratnadeep Ghosh <65853158+RatnadeepGhosh@users.noreply.github.com>
2020-10-02 13:18:51 +0530

    Create The_Minion_Game.py

---
+++ The_Minion_Game.py
@@ -0,0 +1,20 @@
+def minion_game(s):
+    ss=0
+    sk=0
+    for i in range(0, len(s)):
+        add=len(s)-i
+        if s[i] in ['A','E','I','O','U']:
+            sk +=add
+        else:
+            ss +=add
+            
+    if ss>sk:
+        print('Stuart '+str(ss))
+    elif ss<sk:
+        print("Kevin "+str(sk))
+    else:
+        print("Draw")  
+        
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
